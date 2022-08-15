33849f6fed2dba2a546a74fd950fb7524ad56236
«SERGEY» <szely@yandex>
2022-08-15 20:37:51 +0300

    Task_07

---
+++ GB/4. Знакомвство с языками программирования/Семинары/1./TASK2/Task_07/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4  -> 24
+// 5  -> 120
+
+using static System.Console;
+Clear();
+Write("Введите число: ");
+int number = int.Parse(ReadLine());
+int sum = GetSum(number);
+WriteLine($"Сумма от 1 до {number} = {sum}");
+
+
+
+int GetSum(int number)
+{
+    int result = 1;
+    for(int i = 1; i<=number; i++)
+    {
+        result*=i;
+    }
+    return result;
+}
+
+
